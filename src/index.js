30ce2ed82efaa5c6161996cff15cc361a7cd9a48
Laxman Desai <lolopaw24@gmail.com>
2022-05-24 19:31:31 +0530

    Add middle ellipsis

--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ var activeNavItem = undefined;
 var navItems = [];
 var data = [];
 
+// ? Generates title displayed in the nav item
+// * Add middle ellepsis and replace " " with "&nbsp;"
+const getNavItemTitle = (_title) => {
+  const title = _title.replace(/ /g, "\u00a0");
+
+  return `
+    <span class="firstPart">
+      ${title.slice(0, -7)}
+    </span>
+    <span class="lastPart">
+      ${title.slice(-7).replace(/ /g, "\u00a0")}
+    </span>
+  `;
+};
+
+// ? Generates a nav item
+const getNavItem = ({ previewImage, title }) => `
+  <li class="nav-item">
+    <img class="nav-item--img" src="${previewImage}"></img>
+    <div class="nav-item--title">${getNavItemTitle(title)}</div>
+  </li>
+`;
+
+// ? Makes nav item with given index active
 const makeActive = (index) => {
   const itemLen = navItems.length;
   if (index < 0) {
@@ -25,35 +49,30 @@ const makeActive = (index) => {
   previewTitle.value = data[activeNavItem].title;
 };
 
+// ? Initializes the nav from the given JSON data
 const loadNavigation = async () => {
   const response = await fetch("./data.json");
   data = await response.json();
 
   data.forEach((item) => {
-    const navItem = document.createElement("li");
-    navItem.className = "nav-item";
-    navItem.innerHTML = `
-      <img class="nav-item--img" src="${item.previewImage}"></img>
-      <div class="nav-item--title" data-filetype=${item.title.slice(-7)}>
-        <span>${item.title}</span>
-      </div>
-    `;
-
-    nav.appendChild(navItem);
+    nav.insertAdjacentHTML("beforeend", getNavItem(item));
   });
 
   navItems = document.querySelectorAll(".nav-item");
   navItems.forEach((item, index) => {
+    // ? Click event to make nav item active
     item.addEventListener("click", () => makeActive(index));
   });
 
+  // ? By default active nav item is the first one
   makeActive(0);
 };
 
 document.addEventListener("DOMContentLoaded", loadNavigation);
 
-// ? if user presses up or down arrow, make it active
+// ? if user presses up or down arrow, make nav item active
 document.addEventListener("keydown", (event) => {
+  event.preventDefault();
   if (event.key === "ArrowUp") {
     makeActive(activeNavItem - 1);
   } else if (event.key === "ArrowDown") {
@@ -61,8 +80,15 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+// ? If title editor is active, don't allow user to navigate with arrow keys
+previewTitle.addEventListener("keydown", (event) => {
+  event.stopPropagation();
+});
+
+// ? Change title when user types in the title editor
 previewTitle.addEventListener("input", (event) => {
-  data[activeNavItem].title = event.target.value;
+  const title = event.target.value;
+  data[activeNavItem].title = title;
   navItems[activeNavItem].querySelector(".nav-item--title").innerHTML =
-    event.target.value;
+    getNavItemTitle(title);
 });
